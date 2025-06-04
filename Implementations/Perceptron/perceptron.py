62a68d0ce6d196e21d17f72dd4cdb7f61708b512
CllxLima <96326019+CllsPy@users.noreply.github.com>
2025-06-04 13:11:22 +0000

    add: fit function

--- Implementations/Perceptron/perceptron.py
+++ Implementations/Perceptron/perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Perceptron:
     def __init__(self, learning_rate=0.01, n_iterations=1000):
         self.learning_rate = learning_rate
@@ -5,15 +8,22 @@ class Perceptron:
         self.bias = None
         self.weights = None
     
-    def activation_function(self, x, thershold: int = 0):
-        return np.where(x > thershold, 1, 0)
-
-
-        
+    def _step_function(self, x, threshold: int = 0):
+        return np.where(x > threshold, 1, 0)
     
-x = [1, 2, 3, -1]
+    def fit(self, X, y):
+        n_samples, n_featues = X.shape
 
-neuron = Perceptron()
-neuron.activation_function(x)
+        self.weights = np.zeros(n_featues)
+        self.bias = 0
 
-print((lambda x: x**2)(10))
+        for _ in range(n_iterations):
+            for i in range(n_samples):
+                z = np.dot(X[i], self.weights) + self.bias
+            
+            #  apply activation function
+            y = self._step_function(z)
+
+            #  update w and b
+            self.weights += self.learning_rate * (y[i] - y_pred) * X[i)
+            self.bias += self.learning_rate * (y[i] - y_pred)
